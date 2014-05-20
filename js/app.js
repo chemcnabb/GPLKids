94762f940813e4c3b7483062d4d6e55bce6d8d71
Che Mcnabb <chemcnabb@gmail.com>
2014-05-20 17:17:43 -0400

    foundation-ifying

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+$("#tabs").idTabs(function (id, list, set) {
+    $("a", set).removeClass("selected")
+        .filter("[href='" + id + "']", set).addClass("selected");
+    for (i in list)
+        $(list[i]).hide();
+    $(id).fadeIn();
+    return false;
+});
+
+$( "#tab-heading").find("a" ).click(function( event ) {
+    event.preventDefault();
+});
